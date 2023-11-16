72f7c9f6a7fd1ace8397af644f901b2067e240d3
Shraddha87Jha <shraddhajha62@gmail.com>
2023-11-16 13:03:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Offers from "./pages/Offers";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Forgotpassword.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Forgotpassword = () => {
+  return (
+    <Layout>
+      <h1>Forgotpassword page</h1>
+    </Layout>
+  );
+};
+
+export default Forgotpassword;
